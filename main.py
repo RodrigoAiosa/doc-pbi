8a2495b92f65ab0608bc6998966eba75e761ebea
Rodrigo Aiosa <32776696+RodrigoAiosa@users.noreply.github.com>
2026-05-24 15:59:41 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from services.pbix_extractor import PBIXExtractor
+
+st.set_page_config(
+    page_title="Power BI Documenter",
+    layout="wide"
+)
+
+st.title("📘 Power BI Documentation Generator")
+
+uploaded_file = st.file_uploader(
+    "Upload do arquivo PBIX",
+    type=["pbix"]
+)
+
+if uploaded_file:
+
+    with open("temp.pbix", "wb") as f:
+        f.write(uploaded_file.read())
+
+    extractor = PBIXExtractor("temp.pbix")
+
+    extracted = extractor.extract()
+
+    st.success("Arquivo processado com sucesso!")
